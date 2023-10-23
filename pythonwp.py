070c50d0fc966a78872ce56efa1effd66099c5b3
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2023-10-23 23:03:07 +0530

    Update pythonwp.py

--- pythonwp.py
+++ pythonwp.py
@@ -3,7 +3,7 @@ import pywhatkit as pw
 from datetime import datetime, timedelta
 import time
 
-MobilenNo = "+91 77081 96973"
+MobilenNo = "+91 00000 00000"
 
 # Get the current date and time
 current_time = datetime.now()
@@ -57,22 +57,22 @@ new_time = current_time + timedelta(minutes=1)
 # Extract the updated hours and minutes
 hour = new_time.hour
 minute = new_time.minute
-MobilenNo = "+91 7339429677"
+MobilenNo = "+91 00000 00000"
 
 # pw.sendwhatmsg(
 #     "+91 77081 96973", "hey bro...  \nmsg send by python ;)", hour, minute, 15
 # )
 
-pw.sendwhatmsg("+91 77081 96973", "               ", hour, minute, 15)
+pw.sendwhatmsg("+91 00000 00000", "               ", hour, minute, 15)
 
 # Define the path to the image you want to send
-image_path = r"D:\sample proj\wp\gow.png"
+image_path = r"gow.png"
 
 # Define the caption for the image
 caption = "image by Python 'GOW' test1"
 
 try:
-    pw.sendwhats_image("+91 77081 96973", image_path, caption)
+    pw.sendwhats_image("+91 00000 00000", image_path, caption)
     print("Image sent successfully!")
 except Exception as e:
     print(f"An error occurred while sending the image: {str(e)}")
