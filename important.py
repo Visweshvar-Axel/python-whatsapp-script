9527ce1c835af241468cb263026b29cb94359d29
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2023-10-28 18:28:00 +0530

    Add files via upload

---
+++ important.py
@@ -0,0 +1,85 @@
+import os
+import time
+
+
+def get_image_file_names(folder_path):
+    image_files = [
+        f
+        for f in os.listdir(folder_path)
+        if f.lower().endswith((".jpg", ".jpeg", ".png", ".gif"))
+    ]
+    return image_files
+
+
+folder_path = r"D:\sample proj\wp\image_folder"
+
+import pywhatkit as pw
+from datetime import datetime, timedelta
+
+# Get the current date and time
+current_time = datetime.now()
+
+# Extract hours and minutes
+current_hour = current_time.hour
+current_minute = current_time.minute
+
+# Add one minute
+new_time = current_time + timedelta(minutes=1)
+
+# Extract the updated hours and minutes
+hour = new_time.hour
+minute = new_time.minute
+
+
+MobilenNo = "+91 77081 96973"
+image_names = get_image_file_names(folder_path)
+count = 1
+
+pw.sendwhatmsg(MobilenNo, "               ", hour, minute, 15)
+
+
+for names in image_names:
+    # Get the current date and time
+    current_time = datetime.now()
+
+    # Extract hours and minutes
+    current_hour = current_time.hour
+    current_minute = current_time.minute
+
+    # Add one minute
+    new_time = current_time + timedelta(minutes=1)
+
+    # Extract the updated hours and minutes
+    hour = new_time.hour
+    minute = new_time.minute
+
+    image_path = folder_path + "\\" + names
+    try:
+        pw.sendwhats_image(MobilenNo, image_path, str(count))
+        print("Image sent successfully!")
+    except Exception as e:
+        print(f"An error occurred while sending the image: {str(e)}")
+    count += 1
+    time.sleep(1.5)
+
+# Get the current date and time
+current_time = datetime.now()
+
+# Extract hours and minutes
+current_hour = current_time.hour
+current_minute = current_time.minute
+
+# Add one minute
+new_time = current_time + timedelta(minutes=1)
+
+# Extract the updated hours and minutes
+hour = new_time.hour
+minute = new_time.minute
+
+pw.sendwhatmsg(
+    MobilenNo,
+    "Images are send by AI powered bot...\n Enjoy your clicks",
+    hour,
+    minute,
+    15,
+)
